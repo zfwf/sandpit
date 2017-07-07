7268846e1de95a5b9066303f1c7ce5711e3e2f47
Chris Chou <chhschou@gmail.com>
2017-07-07 18:10:40 +1200

    now able to rotate with mouse

--- opengl/web/3dcube/main.js
+++ opengl/web/3dcube/main.js
@@ -112,119 +112,51 @@ function main() {
     lib.lenVec3 * lib.sizeofFloat32);
 
 
-  // matrices
-  let m4 = {
-    multiply: function (a, b) {
-      var a00 = a[0 * 4 + 0];
-      var a01 = a[0 * 4 + 1];
-      var a02 = a[0 * 4 + 2];
-      var a03 = a[0 * 4 + 3];
-      var a10 = a[1 * 4 + 0];
-      var a11 = a[1 * 4 + 1];
-      var a12 = a[1 * 4 + 2];
-      var a13 = a[1 * 4 + 3];
-      var a20 = a[2 * 4 + 0];
-      var a21 = a[2 * 4 + 1];
-      var a22 = a[2 * 4 + 2];
-      var a23 = a[2 * 4 + 3];
-      var a30 = a[3 * 4 + 0];
-      var a31 = a[3 * 4 + 1];
-      var a32 = a[3 * 4 + 2];
-      var a33 = a[3 * 4 + 3];
-      var b00 = b[0 * 4 + 0];
-      var b01 = b[0 * 4 + 1];
-      var b02 = b[0 * 4 + 2];
-      var b03 = b[0 * 4 + 3];
-      var b10 = b[1 * 4 + 0];
-      var b11 = b[1 * 4 + 1];
-      var b12 = b[1 * 4 + 2];
-      var b13 = b[1 * 4 + 3];
-      var b20 = b[2 * 4 + 0];
-      var b21 = b[2 * 4 + 1];
-      var b22 = b[2 * 4 + 2];
-      var b23 = b[2 * 4 + 3];
-      var b30 = b[3 * 4 + 0];
-      var b31 = b[3 * 4 + 1];
-      var b32 = b[3 * 4 + 2];
-      var b33 = b[3 * 4 + 3];
-      return [
-        b00 * a00 + b01 * a10 + b02 * a20 + b03 * a30,
-        b00 * a01 + b01 * a11 + b02 * a21 + b03 * a31,
-        b00 * a02 + b01 * a12 + b02 * a22 + b03 * a32,
-        b00 * a03 + b01 * a13 + b02 * a23 + b03 * a33,
-        b10 * a00 + b11 * a10 + b12 * a20 + b13 * a30,
-        b10 * a01 + b11 * a11 + b12 * a21 + b13 * a31,
-        b10 * a02 + b11 * a12 + b12 * a22 + b13 * a32,
-        b10 * a03 + b11 * a13 + b12 * a23 + b13 * a33,
-        b20 * a00 + b21 * a10 + b22 * a20 + b23 * a30,
-        b20 * a01 + b21 * a11 + b22 * a21 + b23 * a31,
-        b20 * a02 + b21 * a12 + b22 * a22 + b23 * a32,
-        b20 * a03 + b21 * a13 + b22 * a23 + b23 * a33,
-        b30 * a00 + b31 * a10 + b32 * a20 + b33 * a30,
-        b30 * a01 + b31 * a11 + b32 * a21 + b33 * a31,
-        b30 * a02 + b31 * a12 + b32 * a22 + b33 * a32,
-        b30 * a03 + b31 * a13 + b32 * a23 + b33 * a33,
-      ];
-    },
-    translate: function (x, y, z) {
-      return [
-        1, 0, 0, 0,
-        0, 1, 0, 0,
-        0, 0, 1, 0,
-        x, y, z, 1,
-      ]
-    },
-    xRotation: function (angleInRad) {
-      let c = Math.cos(angleInRad);
-      let s = Math.sin(angleInRad);
-      return [
-        1, 0, 0, 0,
-        0, c, s, 0,
-        0, -s, c, 0,
-        0, 0, 0, 1,
-      ]
-    },
-    yRotation: function (angleInRad) {
-      let c = Math.cos(angleInRad);
-      let s = Math.sin(angleInRad);
-      return [
-        c, 0, -s, 0,
-        0, 1, 0, 0,
-        s, 0, c, 0,
-        0, 0, 0, 1,
-      ]
-    },
-    zRotation: function (angleInRad) {
-      let c = Math.cos(angleInRad);
-      let s = Math.sin(angleInRad);
-      return [
-        c, s, 0, 0, -s, c, 0, 0,
-        0, 0, 1, 0,
-        0, 0, 0, 1,
-      ]
-    },
-    xRotate: function (m, angleInRad) {
-      return m4.multiply(m,m4.xRotation(angleInRad))
-    },
-    yRotate: function (m, angleInRad) {
-      return m4.multiply(m,m4.yRotation(angleInRad))
-    },
-    zRotate: function (m, angleInRad) {
-      return m4.multiply(m,m4.zRotation(angleInRad))
-    },
+
+  let rotation = {
+    x: 0,
+    y: 0,
+    z: 0,
   }
+
+  let previousMousePosition = {
+    x: 0,
+    y: 0
+  }
+
+  let isDragging = false;
+
+  viewport.addEventListener("mousedown", function (e) {
+    isDragging = true;
+  })
+  viewport.addEventListener("mouseup", function (e) {
+    isDragging = false;
+  })
+  viewport.addEventListener("mousemove", function (e) {
+    if (isDragging) {
+      let deltaMove = {
+        x: e.offsetX - previousMousePosition.x,
+        y: e.offsetY - previousMousePosition.y,
+      }
+
+      console.log(rotation)
+      rotation.x += toRadian(deltaMove.x)
+      rotation.y += toRadian(deltaMove.y)
+    }
+  })
+
 
 
 
   let matrixAddr = gl.getUniformLocation(shaderProgram, "mvp");
-  let rotateby = 0;
 
   let render = function () {
     lib.resizeMaintainAspect(viewport)
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
     // rotate x by 1 deg per frame, takes 3 second to complete a full 360 deg rotation
-    let mvp = m4.xRotation(Math.PI * (rotateby / 180))
+    let mvp = m4.xRotate(m4.identity(), rotation.x)
+    mvp = m4.yRotate(mvp, rotation.y)
     gl.uniformMatrix4fv(matrixAddr, false, mvp);
 
     gl.bindVertexArray(vao);
@@ -234,9 +166,121 @@ function main() {
 
     gl.flush();
     window.requestAnimationFrame(render);
-    rotateby++
-    if (rotateby == 360) rotateby = 0
   };
 
   render();
+}
+
+function toRadian(angleInDeg) {
+  return Math.PI * (angleInDeg / 180)
+}
+
+// matrices
+let m4 = {
+  multiply: function (a, b) {
+    var a00 = a[0 * 4 + 0];
+    var a01 = a[0 * 4 + 1];
+    var a02 = a[0 * 4 + 2];
+    var a03 = a[0 * 4 + 3];
+    var a10 = a[1 * 4 + 0];
+    var a11 = a[1 * 4 + 1];
+    var a12 = a[1 * 4 + 2];
+    var a13 = a[1 * 4 + 3];
+    var a20 = a[2 * 4 + 0];
+    var a21 = a[2 * 4 + 1];
+    var a22 = a[2 * 4 + 2];
+    var a23 = a[2 * 4 + 3];
+    var a30 = a[3 * 4 + 0];
+    var a31 = a[3 * 4 + 1];
+    var a32 = a[3 * 4 + 2];
+    var a33 = a[3 * 4 + 3];
+    var b00 = b[0 * 4 + 0];
+    var b01 = b[0 * 4 + 1];
+    var b02 = b[0 * 4 + 2];
+    var b03 = b[0 * 4 + 3];
+    var b10 = b[1 * 4 + 0];
+    var b11 = b[1 * 4 + 1];
+    var b12 = b[1 * 4 + 2];
+    var b13 = b[1 * 4 + 3];
+    var b20 = b[2 * 4 + 0];
+    var b21 = b[2 * 4 + 1];
+    var b22 = b[2 * 4 + 2];
+    var b23 = b[2 * 4 + 3];
+    var b30 = b[3 * 4 + 0];
+    var b31 = b[3 * 4 + 1];
+    var b32 = b[3 * 4 + 2];
+    var b33 = b[3 * 4 + 3];
+    return [
+      b00 * a00 + b01 * a10 + b02 * a20 + b03 * a30,
+      b00 * a01 + b01 * a11 + b02 * a21 + b03 * a31,
+      b00 * a02 + b01 * a12 + b02 * a22 + b03 * a32,
+      b00 * a03 + b01 * a13 + b02 * a23 + b03 * a33,
+      b10 * a00 + b11 * a10 + b12 * a20 + b13 * a30,
+      b10 * a01 + b11 * a11 + b12 * a21 + b13 * a31,
+      b10 * a02 + b11 * a12 + b12 * a22 + b13 * a32,
+      b10 * a03 + b11 * a13 + b12 * a23 + b13 * a33,
+      b20 * a00 + b21 * a10 + b22 * a20 + b23 * a30,
+      b20 * a01 + b21 * a11 + b22 * a21 + b23 * a31,
+      b20 * a02 + b21 * a12 + b22 * a22 + b23 * a32,
+      b20 * a03 + b21 * a13 + b22 * a23 + b23 * a33,
+      b30 * a00 + b31 * a10 + b32 * a20 + b33 * a30,
+      b30 * a01 + b31 * a11 + b32 * a21 + b33 * a31,
+      b30 * a02 + b31 * a12 + b32 * a22 + b33 * a32,
+      b30 * a03 + b31 * a13 + b32 * a23 + b33 * a33,
+    ];
+  },
+  identity: function () {
+    return [
+      1, 0, 0, 0,
+      0, 1, 0, 0,
+      0, 0, 1, 0,
+      0, 0, 0, 1,
+    ]
+  },
+  translate: function (x, y, z) {
+    return [
+      1, 0, 0, 0,
+      0, 1, 0, 0,
+      0, 0, 1, 0,
+      x, y, z, 1,
+    ]
+  },
+  xRotation: function (angleInRad) {
+    let c = Math.cos(angleInRad);
+    let s = Math.sin(angleInRad);
+    return [
+      1, 0, 0, 0,
+      0, c, s, 0,
+      0, -s, c, 0,
+      0, 0, 0, 1,
+    ]
+  },
+  yRotation: function (angleInRad) {
+    let c = Math.cos(angleInRad);
+    let s = Math.sin(angleInRad);
+    return [
+      c, 0, -s, 0,
+      0, 1, 0, 0,
+      s, 0, c, 0,
+      0, 0, 0, 1,
+    ]
+  },
+  zRotation: function (angleInRad) {
+    let c = Math.cos(angleInRad);
+    let s = Math.sin(angleInRad);
+    return [
+      c, s, 0, 0, -s, c, 0, 0,
+      0, 0, 1, 0,
+      0, 0, 0, 1,
+    ]
+  },
+  xRotate: function (m, angleInRad) {
+    return m4.multiply(m, m4.xRotation(angleInRad))
+  },
+  yRotate: function (m, angleInRad) {
+    return m4.multiply(m, m4.yRotation(angleInRad))
+  },
+  zRotate: function (m, angleInRad) {
+    return m4.multiply(m, m4.zRotation(angleInRad))
+  },
 }
